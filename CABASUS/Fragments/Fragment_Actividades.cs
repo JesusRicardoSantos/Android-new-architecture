4fb76df945105485358818019dd6d499db8f460f
Alejandro Ramírez Chía <RACA_951106@hotmail.com>
2019-05-03 16:54:30 -0500

    imagenes circulares y llenado de lista de actividades

--- CABASUS/Fragments/Fragment_Actividades.cs
+++ CABASUS/Fragments/Fragment_Actividades.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
 using Android.Views;
-using Android.Widget;
 
 namespace CABASUS.Fragments
 {
@@ -21,22 +13,7 @@ namespace CABASUS.Fragments
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            View Vista = inflater.Inflate(Resource.Layout.layout_actividades, container, false);
-
-            //FragmentTransaction transaccion;
-            //fragment_caballos_actividades fragment_caballos_actividades = new fragment_caballos_actividades();
-            //fragment_lista_actividades fragment_lista_actividades = new fragment_lista_actividades();
-
-            //transaccion = FragmentManager.BeginTransaction();
-            //transaccion.Add(Resource.Id.FrameContent, fragment_caballos_actividades, "fragment_caballos_actividades");
-            //transaccion.Show(fragment_caballos_actividades);
-            //transaccion.Commit();
-
-            //transaccion = FragmentManager.BeginTransaction();
-            //transaccion.Add(Resource.Id.FrameContent, fragment_lista_actividades, "fragment_lista_actividades");
-            //transaccion.Show(fragment_lista_actividades);
-            //transaccion.Commit();
-                        
+            View Vista = inflater.Inflate(Resource.Layout.layout_actividades, container, false);                        
             return Vista;
         }
     }
--- CABASUS/Fragments/fragment_lista_actividades.cs
+++ CABASUS/Fragments/fragment_lista_actividades.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
+using Android.Graphics;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
+using System;
 
 namespace CABASUS.Fragments
 {
@@ -14,6 +17,47 @@ namespace CABASUS.Fragments
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             View Vista = inflater.Inflate(Resource.Layout.layout_lista_actividades, container, false);
+
+            //agregar fecha al primero eliminar esto cuando la logica sea la chida
+            var primero = true;
+            TextView txtFecha = new TextView(Context);
+            var LayoutParameters = new LinearLayout.LayoutParams(-1, ViewGroup.LayoutParams.WrapContent);
+            LayoutParameters.SetMargins(0, 20, 0, 20);
+            txtFecha.LayoutParameters = LayoutParameters;
+            txtFecha.Text = DateTime.Now.ToString("dd MMMM yyyy");
+            txtFecha.TextAlignment = TextAlignment.Center;
+            txtFecha.SetTextColor(Color.Black);
+            txtFecha.TextSize = 22;
+            txtFecha.SetTypeface(null, TypefaceStyle.Italic);
+
+            Vista.FindViewById<LinearLayout>(Resource.Id.listaActividades).AddView(txtFecha);
+
+            for (int i = 0; i < 20; i++)
+            {
+                //vista para colocar la fecha
+                Random r = new Random();
+
+                if (r.Next(0, 10) >= 5 && !primero)
+                {
+                    txtFecha = new TextView(Context);
+                    LayoutParameters = new LinearLayout.LayoutParams(-1, ViewGroup.LayoutParams.WrapContent);
+                    LayoutParameters.SetMargins(0, 20, 0, 20);
+                    txtFecha.LayoutParameters = LayoutParameters;
+                    txtFecha.Text = DateTime.Now.AddDays(i).ToString("dd MMMM yyyy");
+                    txtFecha.TextAlignment = TextAlignment.Center;
+                    txtFecha.SetTextColor(Color.Black);
+                    txtFecha.TextSize = 22;
+                    txtFecha.SetTypeface(null, TypefaceStyle.Italic);
+
+                    Vista.FindViewById<LinearLayout>(Resource.Id.listaActividades).AddView(txtFecha);
+                }
+
+                //vista con datos para cada actividad
+                var layout = inflater.Inflate(Resource.Layout.ItemActividad, container, false);
+                Vista.FindViewById<LinearLayout>(Resource.Id.listaActividades).AddView(layout);
+
+                primero = false;
+            }
             return Vista;
         }
     }
